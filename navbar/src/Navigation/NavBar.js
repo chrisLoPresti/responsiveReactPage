8daa260a657365adac0f11bb2ceaf353a2d5c0dd
Chris LoPresti <lopresti.chris@yahoo.com>
2018-07-01 21:26:57 -0400

    navbar react

--- navbar/src/Navigation/NavBar.js
+++ navbar/src/Navigation/NavBar.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Grid from '@material-ui/core/Grid';
 import Drawer from '@material-ui/core/Drawer';
+import Fade from '@material-ui/core/Fade';
+import Zoom from '@material-ui/core/Zoom';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Paper from '@material-ui/core/Paper';
+import Grow from '@material-ui/core/Grow';
 import './NavBar.css'
 import 'font-awesome/css/font-awesome.min.css';
-import SideNav from './test.js';
+
 const filters = [
   {
-    name: 'Finn'
+    name: 'x'
   }, {
-    name: 'Chase Mobile'
+    name: 'y'
   }, {
-    name: 'Ink'
+    name: 'z'
   }, {
-    name: 'Chase Online'
+    name: 'a'
   }, {
-    name: 'QuickPay'
+    name: 'b'
   }
 ];
+
 const settings = [
   {
     name: 'Favorites'
@@ -37,6 +41,20 @@ const settings = [
   }
 ];
 
+function renderFilters() {
+
+  return (<Grid container="container">
+    <Grid className="grid" item="item" xs={1}/> {
+      filters.map(filter => (<Grid key={filter.name} className="grid" item="item" xs={2}>
+        <Typography className="toolbar-filter" color="inherit">
+          {filter.name}
+        </Typography>
+      </Grid>))
+    }
+    <Grid className="grid" item="item" xs={1}/>
+  </Grid>);
+};
+
 class NavBar extends React.Component {
 
   constructor() {
@@ -46,7 +64,8 @@ class NavBar extends React.Component {
     this.state = {
 
       renderSearch: false,
-      renderSideNav: false
+      renderSideNav: false,
+      renderDropMenu: false
     };
 
     this.handleResizeCheck = this.handleResizeCheck.bind(this);
@@ -57,8 +76,11 @@ class NavBar extends React.Component {
   }
 
   handleResizeCheck() {
-    if (window.innerWidth >= 600) {
+    if (window.innerWidth >= 825) {
       this.setState({renderSideNav: false});
+    } else {
+      this.setState({renderDropMenu: false, renderSearch: false});
+
     }
   }
 
@@ -80,69 +102,134 @@ class NavBar extends React.Component {
 
   }
 
-  renderSearchOrLogo() {
+  handleToggleDropMenu = () => {
+
+    this.setState(prevState => ({
+
+      renderDropMenu: !prevState.renderDropMenu
+    }));
+
+  }
+
+  renderSearchBar() {
 
     if (this.state.renderSearch) {
-
-      return (<div id="search-bar-container">
-        <input id="search-bar" type="search" placeholder="Search.."/></div>);
+      return (<Fade in={this.state.renderSearch} direction="left" timeout={1000}>
+        <div id="search-bar-container">
+          <input id="search-bar" type="search" placeholder="Search.."/>
+          <IconButton onClick={this.handleToggleSearch} id="search-icon" aria-label="Menu">
+            <i className="fa fa-search"></i>
+          </IconButton>
+        </div>
+      </Fade>);
     } else {
-      return (<div id="logo-container"></div>);
+      return (<div id="search-bar-container">
+        <IconButton onClick={this.handleToggleSearch} id="search-icon" aria-label="Menu">
+          <i className="fa fa-search"></i>
+        </IconButton>
+      </div>);
     }
-
-  };
-
-  renderFilters() {
-
-    return (<Grid container="container">
-      <Grid className="grid" item="item" xs={1}/> {
-        filters.map(filter => (<Grid key={filter.name} className="grid" item="item" xs={2}>
-
-          <Typography className="toolbar-filter" color="inherit">
-            {filter.name}
-
-          </Typography>
-
-        </Grid>))
-      }
-
-      <Grid className="grid" item="item" xs={1}/>
-    </Grid>);
-  };
+  }
 
   renderSideNav() {
-    if (this.state.renderSideNav) {
-      return (<Drawer className="drawer" variant="temporary" open={this.state.renderSideNav} onClose={this.handleToggleSideNav}>
-        <div id="side-nav-container" tabIndex={0} role="button" onKeyDown={this.handleToggleSideNav}>
 
+    return (<Drawer className="drawer" variant="temporary" open={this.state.renderSideNav} onClose={this.handleToggleSideNav}>
+      <div id="side-nav-container">
+        <Grow in={this.state.renderSideNav}>
           <Typography id="category">
             Filter By:
           </Typography>
+        </Grow>
 
-          <hr/> {
-            filters.map(filter => (<div class="side-filter-container">
+        <hr/> {
+          filters.map((filter, i) => (<div key={filter.name} class="side-filter-container">
+            <Grow in={this.state.renderSideNav} style={{
+                transformOrigin: '0 0 0'
+              }} {...(this.state.renderSideNav ? { timeout: 200 * (i + 1)} : {})}>
               <Typography id="side-filter">
                 {filter.name}
 
               </Typography>
-              <hr/></div>))
-          }
-
+            </Grow>
+            <hr/></div>))
+        }
+        <Grow in={this.state.renderSideNav} style={{
+            transformOrigin: '0 0 0'
+          }} {...(this.state.renderSideNav ? { timeout: 200 * (filters.length + 1)} : {})}>
           <Typography id="category">
             My Stuff:
           </Typography>
+        </Grow>
 
-          <hr/>{
-            settings.map(item => (<div class="side-filter-container">
+        <hr/>{
+          settings.map((item, i) => (<div key={item.name} class="side-filter-container">
+            <Grow in={this.state.renderSideNav} style={{
+                transformOrigin: '0 0 0'
+              }} {...(this.state.renderSideNav ? { timeout: 200 * (filters.length + i + 2)} : {})}>
               <Typography id="side-filter">
                 {item.name}
 
               </Typography>
-              <hr/></div>))
-          }
+            </Grow>
+            <hr/></div>))
+        }
+      </div>
+    </Drawer>);
 
+  }
+
+  renderLogo() {
+
+    return (<Grid className="grid" item="item" xs={this.determineInnerGridSize()}>
+      <Zoom in={true}>
+        <div id="logo-container"></div>
+      </Zoom>
+    </Grid>);
+
+  }
+
+  renderDropMenu() {
+    if (this.state.renderDropMenu) {
+      return (<Fade in={this.state.renderDropMenu} timeout={500} id="menu-key-down" style={{
+          transformOrigin: '0 0 0'
+        }}>
+        <div id="menu-key-down">
+          <div id="drop-menu">
+            <Paper style={{
+                margin: 3
+              }}>
+              <MenuList>
+                {
+                  settings.map(item => (<MenuItem key={item.name} id="drop-menu-item" onClick={this.handleToggleDropMenu}>
+                    {item.name}
+                  </MenuItem>))
+                }
+              </MenuList>
+            </Paper>
+
+          </div>
         </div>
-      </Drawer>);
+      </Fade>);
+    } else {
+      return null;
+    }
+  }
+
+  determineOuterGridSize() {
+
+    if (this.state.renderSearch) {
+      return 5;
+    } else {
+      return 4;
+    }
+  }
+
+  determineInnerGridSize() {
+
+    if (this.state.renderSearch) {
+      return 2;
+    } else {
+      return 4;
     }
   }
 
@@ -151,7 +238,7 @@ class NavBar extends React.Component {
       <AppBar position="static" id="appBar">
         <Toolbar id="top-row">
           <Grid container="container">
-            <Grid className="grid" item="item" xs={4}>
+            <Grid className="grid" item="item" xs={this.determineOuterGridSize()}>
               <div id="menu-container">
                 <IconButton id="hamburger" aria-label="Menu" onClick={this.handleToggleSideNav}>
                   <MenuIcon/>
@@ -161,26 +248,23 @@ class NavBar extends React.Component {
                 </Typography>
               </div>
             </Grid>
-            <Grid className="grid" item="item" xs={4}>
-              {this.renderSearchOrLogo()}
-            </Grid>
-            <Grid className="grid" item="item" xs={4}>
+            {this.renderLogo()}
+            <Grid className="grid" item="item" xs={this.determineOuterGridSize()}>
               <div id="icon-container">
-                <IconButton onClick={this.handleToggleSearch} id="search-icon" aria-label="Menu">
-                  <i className="fa fa-search"></i>
-                </IconButton>
                 <IconButton id="heart-icon" aria-label="Menu">
                   <i className="fa fa-heart"></i>
                 </IconButton>
-                <IconButton id="settings-icon" aria-label="Menu">
+                <IconButton id="settings-icon" aria-label="Menu" onClick={this.handleToggleDropMenu}>
                   <i className="fa fa-cog"></i>
                 </IconButton>
+                {this.renderDropMenu()}
               </div>
+              {this.renderSearchBar()}
             </Grid>
           </Grid>
         </Toolbar>
         <Toolbar id="bottom-row">
-          {this.renderFilters()}
+          {renderFilters()}
         </Toolbar>
         {this.renderSideNav()}
       </AppBar>
